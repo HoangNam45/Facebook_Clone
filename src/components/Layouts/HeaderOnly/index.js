6f82dbbe47e42adf73babb684fd3d8fab6275076
Nam <nhhnam0405@gmail.com>
2024-07-18 01:45:12 +0700

    header

--- src/components/Layouts/HeaderOnly/index.js
+++ src/components/Layouts/HeaderOnly/index.js
@@ -3,9 +3,9 @@ function HeaderOnly({ children }) {
     return (
         <div>
             <Header />
-            <div className="container">
+            {/* <div className="container">
                 <div className="content">{children}</div>
-            </div>
+            </div> */}
         </div>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-import { FooterOnly } from '../components/Layouts';
+import { FooterOnly, HeaderOnly } from '../components/Layouts';
 import { Home, Login, Register, Test } from '../pages';
 const publicRoutes = [
-    { path: '/', component: Home, layout: FooterOnly },
+    { path: '/', component: Home, layout: HeaderOnly },
     { path: '/login', component: Login, layout: FooterOnly },
     { path: '/register', component: Register, layout: FooterOnly },
     { path: '/test', component: Test, layout: FooterOnly },
